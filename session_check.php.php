ca9445029127c75806b1a0424de8dedf014b5bf6
LELLONONTANTOBELLO <166638110+LELLONONTANTOBELLO@users.noreply.github.com>
2024-09-27 09:38:30 +0200

    Add files via upload

---
+++ session_check.php.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Autocertificati</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css" />
+<link rel="icon" type="image/x-icon" href="https://www.archiviovppay.it/privato/immagini/favicon_180x180.png">
+</head>
+<body>
+<div class="container">
+    <h1>Elenco Certificati</h1>
+    
+    <h4>
+        <fieldset>
+            
+        </fieldset>
+    </h4>
+</div>
+<footer>
+    <div class="footer">
+        <p>Copyright &copy; 2023; Designed by <span class="designer">Roman Modugno</span></p>
+    </div>
+</footer>
+</body>
+</html>
