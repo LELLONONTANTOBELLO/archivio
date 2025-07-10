36b266d9fd6737bbeab93e50a2191dffbc906854
LELLONONTANTOBELLO <166638110+LELLONONTANTOBELLO@users.noreply.github.com>
2025-07-10 17:42:41 +0200

    Delete session_check1.php

--- session_check1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Verifica se l'utente è loggato
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("location: /index.php");
-    exit;
-}
-?>
